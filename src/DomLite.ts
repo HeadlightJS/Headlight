48dfcc146e709962cd77e012889a640ec684e162
Daniil Tsigelnitskiy <d.tsigelnitskiy@corp.mail.ru>
2016-01-29 15:35:53 +0300

    hlt-20: add view

---
+++ src/DomLite.ts
@@ -0,0 +1,76 @@
+module Headlight {
+    'use strict';
+
+    export interface IDomLite {
+        add(selector: string): IDomLite;
+        add(collection: Array<HTMLElement>): IDomLite;
+        addClass(className: string): IDomLite;
+        removeClass(className: string): IDomLite;
+        toggleClass(className: string, state?: boolean): IDomLite;
+    }
+
+    export class DomLite extends Array<HTMLElement> implements IDomLite {
+
+        constructor(selector: string|Array<HTMLElement>) {
+            super();
+            this.add(selector);
+        }
+
+        public add(elements: string|Array<HTMLElement>): IDomLite {
+            if (typeof elements === 'string') {
+                this.__addBySelector(<string>elements);
+            } else {
+                this.__addByArray(<Array<HTMLElement>>elements);
+            }
+            return this;
+        }
+
+        public addClass(className: string): IDomLite {
+            className.split(/\s+/).forEach((classNameItem: string): void => {
+                this.forEach(function (element: HTMLElement): void {
+                    element.classList.add(classNameItem);
+                });
+            });
+            return this;
+        }
+
+        public removeClass(className: string): IDomLite {
+            className.split(/\s+/).forEach((classNameItem: string): void => {
+                this.forEach(function (element: HTMLElement): void {
+                    element.classList.remove(classNameItem);
+                });
+            });
+            return this;
+        }
+
+        public toggleClass(className: string, state?: boolean): IDomLite {
+            switch (state) {
+                case true:
+                    return this.addClass(className);
+                case false:
+                    return this.removeClass(className);
+                default:
+                    this.forEach((element: HTMLElement): void => {
+                        if (element.classList.contains(className)) {
+                            element.classList.remove(className);
+                        } else {
+                            element.classList.add(className);
+                        }
+                    });
+                    return this;
+            }
+        }
+
+        private __addBySelector(selector: string): void {
+            this.__addByArray(Array.prototype.slice.call(document.querySelectorAll(selector)));
+        }
+
+        private __addByArray(elements: Array<HTMLElement>): void {
+            elements.forEach((element: HTMLElement): void => {
+                this.push(element);
+            });
+        }
+
+    }
+
+}
--- src/View.ts
+++ src/View.ts
@@ -4,21 +4,177 @@ module Headlight {
     'use strict';
 
     export interface IView {
+    }
+
+    export interface IDomHandler {
+        (event?: MouseEvent|KeyboardEvent|Event|TouchEvent, target?: HTMLElement): void;
+    }
+
+    export interface IEventHash {
+        event: string;
+        selector: string;
+        handler: IDomHandler;
+        context?: any;
+    }
+
+    export interface IViewOptions {
+        $placeholder: HTMLElement;
+    }
+
+    export abstract class View implements IView {
+
+        protected el: HTMLElement;
+        private __listeningEvents: IListeningHash = {};
+
+        constructor(options: IViewOptions) {
+            this.__createElement();
+            this.initProps(options);
+            this.__initEvents();
+        }
+
+        protected tagName(): string {
+            return 'DIV';
+        }
+
+        protected className(): string {
+            return '';
+        }
+
+        protected id(): string {
+            return '';
+        }
+
+        protected events(): Array<IEventHash> {
+            return [];
+        }
+
+        protected $(selector: string): HTMLElement[] {
+            return Array.prototype.slice.call(this.el.querySelectorAll(selector));
+        }
+
+        protected abstract initProps(options: IViewOptions): this;
+
+        protected setElement(element: HTMLElement): this {
+            let parent = this.el.parentNode;
+            this.el = element;
+            if (parent) {
+                parent.appendChild(this.el);
+            }
+            return this;
+        }
+
+        protected on(eventName: string, selector: string|void, handler: IDomHandler, context?: any): this {
+
+            if (!this.__listeningEvents[eventName]) {
+                this.__addTypeHandler(eventName);
+            }
+
+            this.__listeningEvents[eventName].listeners.push({
+                selector: selector,
+                context: context,
+                handler: handler
+            });
+
+            return this;
+        }
+
+        protected off(eventName?: string, handler?: IDomHandler): this {
+
+            if (!eventName) {
+                Object.keys(this.__listeningEvents).forEach((localEventName: string) => {
+                    this.el.removeEventListener(localEventName, this.__listeningEvents[localEventName].handler);
+                    delete this.__listeningEvents[localEventName];
+                });
+                return this;
+            }
+
+            if (eventName in this.__listeningEvents) {
+
+                let listData = this.__listeningEvents[eventName];
+
+                if (!handler) {
+                    this.el.removeEventListener(eventName, listData.handler);
+                    delete this.__listeningEvents[eventName];
+                    return this;
+                }
+
+                listData.listeners = listData.listeners.filter((listenerData: IListener): boolean => {
+                    return listenerData.handler === handler;
+                });
+
+                if (!listData.listeners.length) {
+                    this.el.removeEventListener(eventName, listData.handler);
+                    delete this.__listeningEvents[eventName];
+                }
+
+            }
+
+            return this;
+        }
+
+        private __addTypeHandler(eventName: string): void {
+            this.__listeningEvents[eventName] = {
+                listeners: [],
+                handler: (event: any): void => {
+                    this.__startEvent(event, eventName);
+                }
+            };
+        }
+
+        private __startEvent(event: any, eventName: string): void {
+            this.__listeningEvents[eventName].listeners.forEach((listenerData: IListener) => {
+                if (!listenerData.selector) {
+                    listenerData.handler.call(listenerData.context, event, this.el);
+                }  else {
+                    this.$(<string>listenerData.selector).some((element: HTMLElement): boolean => {
+                        if (event.target === element || View.__hasInElement(this.el, element, event.target)) {
+                            listenerData.handler.call(listenerData.context, event, element);
+                            return true;
+                        }
+                    });
+                }
+            });
+        }
+
+        private __createElement(): void {
+            this.el = document.createElement(this.tagName());
+            let className = this.className();
+            let id = this.id();
+            this.el.classList.add(className);
+            this.el.id = id;
+        }
+
+        private __initEvents(): void {
+            let events = this.events();
+            events.forEach((eventData: IEventHash): void => {
+                this.on(eventData.event, eventData.selector, eventData.handler, eventData.context || this);
+            });
+        }
+
+        private static __hasInElement(searchRoot: HTMLElement, parent: HTMLElement, child: HTMLElement): boolean {
+            let node = child.parentNode;
+            while (node !== searchRoot) {
+                if (node === parent) {
+                    return true;
+                }
+                node = node.parentNode;
+            }
+            return false;
+        }
 
     }
 
-    export class View implements IView {
-        protected el: HTMLElement = document.createElement('div');
-
-        constructor() {
-            // dummy for tslint
-            let a = 1;
-
-            a = 2;
-        }
-
-        public $(selector: string): NodeListOf<Element> {
-            return this.el.querySelectorAll(selector);
-        }
+    interface IListeningHash {
+        [event: string]: {
+            listeners: Array<IListener>;
+            handler: (event: any) => void;
+        };
     }
+
+    interface IListener {
+        selector: string|void;
+        context: any;
+        handler: IDomHandler;
+    }
+
 }
